d41cdb631a18897032de7175e295411f206469f4
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-07-16 23:09:37 +0300

    move Strophe.Connection to private variables

--- quickblox.js
+++ quickblox.js
@@ -126,66 +126,64 @@ function signMessage(message, secret) {
 // Browserify exports and dependencies
 require('../../lib/strophe/strophe.min');
 var config = require('../qbConfig');
-
 module.exports = ChatProxy;
 
+// create Strophe Connection object
+var protocol = config.chatProtocol.active === 1 ? config.chatProtocol.bosh : config.chatProtocol.websocket;
+var connection = new Strophe.Connection(protocol);
+// if (config.debug) {
+  connection.xmlInput = function(data) { console.log('[QBChat RECV]:', data); };
+  connection.xmlOutput = function(data) { console.log('[QBChat SENT]:', data); };
+// }
+
 function ChatProxy(service) {
-  var protocol = config.chatProtocol.active === 1 ? config.chatProtocol.bosh : config.chatProtocol.websocket;
-
   this.service = service;
-
-  // create Strophe Connection object
-  this._connection = new Strophe.Connection(protocol);
-  // if (config.debug) {
-    this._connection.rawInput = function(data) { console.log('[QBChat RECV]:', data); };
-    this._connection.rawOutput = function(data) { console.log('[QBChat SENT]:', data); };
-  // }
 }
 
 ChatProxy.prototype.connect = function(params, callback) {
   var self = this, err;
   if (config.debug) { console.log('ChatProxy.connect', params); }
 
-  this._connection.connect(params.jid, params.password, function(status) {
+  connection.connect(params.jid, params.password, function(status) {
     switch (status) {
     case Strophe.Status.ERROR:
       err = getError(422, 'Status.ERROR - An error has occurred');
       callback(err, null);
       break;
     case Strophe.Status.CONNECTING:
-      trace('Connecting');
+      trace('Status.CONNECTING');
       break;
     case Strophe.Status.CONNFAIL:
       err = getError(422, 'Status.CONNFAIL - The connection attempt failed');
       callback(err, null);
       break;
     case Strophe.Status.AUTHENTICATING:
-      trace('Authenticating');
+      trace('Status.AUTHENTICATING');
       break;
     case Strophe.Status.AUTHFAIL:
       err = getError(401, 'Status.AUTHFAIL - The authentication attempt failed');
       callback(err, null);
       break;
     case Strophe.Status.CONNECTED:
-      trace('Connected');
+      trace('Status.CONNECTED');
 
-      self._connection.addHandler(self.onMessageListener, null, 'message', 'chat');
-      self._connection.addHandler(self.onMessageListener, null, 'message', 'groupchat');
-      self._connection.addHandler(self.onIQstanzaListener, null, 'iq', 'result');
-      self._connection.addHandler(self.onPresenceListener, null, 'presence');
+      connection.addHandler(self.onMessageListener, null, 'message', 'chat');
+      connection.addHandler(self.onMessageListener, null, 'message', 'groupchat');
+      connection.addHandler(self.onIQstanzaListener, null, 'iq', 'result');
+      connection.addHandler(self.onPresenceListener, null, 'presence');
       
       callback(null, '');
       break;
     case Strophe.Status.DISCONNECTING:
-      trace('Disconnecting');
+      trace('Status.DISCONNECTING');
       if (typeof self.onDisconnectingListener === 'function')
         self.onDisconnectingListener();
       break;
     case Strophe.Status.DISCONNECTED:
-      trace('Disconnected');
+      trace('Status.DISCONNECTED');
       break;
     case Strophe.Status.ATTACHED:
-      trace('Status.ATTACHED - The connection has been attached');
+      trace('Status.ATTACHED');
       break;
     }
   });
@@ -202,7 +200,7 @@ ChatProxy.prototype.getUserJid = function(id) {
 ---------------------------------------------------------------------- */
 function trace(text) {
   // if (config.debug) {
-    console.log('QBChat:', text);
+    console.log('[QBChat]:', text);
   // }
 }
 
