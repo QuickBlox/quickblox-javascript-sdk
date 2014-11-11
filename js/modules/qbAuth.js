26efac7130dd2dfd2680a7c5280862521808b521
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-11-11 10:47:49 +0200

    [RequireJS] update auth module

--- js/modules/qbAuth.js
+++ js/modules/qbAuth.js
@@ -1,3 +1,5 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
 /*
  * QuickBlox JavaScript SDK
  *
@@ -5,111 +7,113 @@
  *
  */
 
-// Browserify exports and dependencies
-module.exports = AuthProxy;
-var config = require('../qbConfig');
-var Utils = require('../qbUtils');
-var crypto = require('crypto-js/hmac-sha1');
+define(['config', 'Utils', 'crypto-js/hmac-sha1'],
+function(config, Utils, crypto) {
 
-function AuthProxy(service) {
-  this.service = service;
-}
-
-AuthProxy.prototype.createSession = function(params, callback) {
-  var _this = this, message;
-
-  if (typeof params === 'function' && typeof callback === 'undefined') {
-    callback = params;
-    params = {};
+  function AuthProxy(service) {
+    this.service = service;
   }
 
-  // Signature of message with SHA-1 using secret key
-  message = generateAuthMsg(params);
-  message.signature = signMessage(message, config.creds.authSecret);
-  
-  if (config.debug) { console.log('AuthProxy.createSession', message); }
-  this.service.ajax({url: Utils.getUrl(config.urls.session), type: 'POST', data: message},
-                    function(err, res) {
-                      if (err) {
-                        callback(err, null);
-                      } else {
-                        _this.service.setSession(res.session);
-                        callback(null, res.session);
-                      }
-                    });
-};
+  AuthProxy.prototype = {
+    createSession: function(params, callback) {
+      var _this = this, message;
 
-AuthProxy.prototype.destroySession = function(callback) {
-  var _this = this;
-  if (config.debug) { console.log('AuthProxy.destroySession'); }
-  this.service.ajax({url: Utils.getUrl(config.urls.session), type: 'DELETE', dataType: 'text'},
-                    function(err, res) {
-                      if (err) {
-                        callback(err, null);
-                      } else {
-                        _this.service.setSession(null);
-                        callback(null, res);
-                      }
-                    });
-};
+      if (typeof params === 'function' && typeof callback === 'undefined') {
+        callback = params;
+        params = {};
+      }
 
-AuthProxy.prototype.login = function(params, callback) {
-  if (config.debug) { console.log('AuthProxy.login', params); }
-  this.service.ajax({url: Utils.getUrl(config.urls.login), type: 'POST', data: params},
-                    function(err, res) {
-                      if (err) { callback(err, null); }
-                      else { callback(null, res.user); }
-                    });
-};
+      // Signature of message with SHA-1 using secret key
+      message = generateAuthMsg(params);
+      message.signature = signMessage(message, config.creds.authSecret);
+      
+      if (config.debug) { console.log('AuthProxy.createSession', message); }
+      this.service.ajax({url: Utils.getUrl(config.urls.session), type: 'POST', data: message},
+                        function(err, res) {
+                          if (err) {
+                            callback(err, null);
+                          } else {
+                            _this.service.setSession(res.session);
+                            callback(null, res.session);
+                          }
+                        });
+    },
 
-AuthProxy.prototype.logout = function(callback) {
-  if (config.debug) { console.log('AuthProxy.logout'); }
-  this.service.ajax({url: Utils.getUrl(config.urls.login), type: 'DELETE', dataType:'text'}, callback);
-};
+    destroySession: function(callback) {
+      var _this = this;
+      if (config.debug) { console.log('AuthProxy.destroySession'); }
+      this.service.ajax({url: Utils.getUrl(config.urls.session), type: 'DELETE', dataType: 'text'},
+                        function(err, res) {
+                          if (err) {
+                            callback(err, null);
+                          } else {
+                            _this.service.setSession(null);
+                            callback(null, res);
+                          }
+                        });
+    },
 
+    login: function(params, callback) {
+      if (config.debug) { console.log('AuthProxy.login', params); }
+      this.service.ajax({url: Utils.getUrl(config.urls.login), type: 'POST', data: params},
+                        function(err, res) {
+                          if (err) { callback(err, null); }
+                          else { callback(null, res.user); }
+                        });
+    },
 
-/* Private
----------------------------------------------------------------------- */
-function generateAuthMsg(params) {
-  var message = {
-    application_id: config.creds.appId,
-    auth_key: config.creds.authKey,
-    nonce: Utils.randomNonce(),
-    timestamp: Utils.unixTime()
+    logout: function(callback) {
+      if (config.debug) { console.log('AuthProxy.logout'); }
+      this.service.ajax({url: Utils.getUrl(config.urls.login), type: 'DELETE', dataType:'text'}, callback);
+    }
   };
-  
-  // With user authorization
-  if (params.login && params.password) {
-    message.user = {login: params.login, password: params.password};
-  } else if (params.email && params.password) {
-    message.user = {email: params.email, password: params.password};
-  } else if (params.provider) {
-    // Via social networking provider (e.g. facebook, twitter etc.)
-    message.provider = params.provider;
-    if (params.scope) {
-      message.scope = params.scope;
-    }
-    if (params.keys && params.keys.token) {
-      message.keys = {token: params.keys.token};
-    }
-    if (params.keys && params.keys.secret) {
-      messages.keys.secret = params.keys.secret;
-    }
-  }
-  
-  return message;
-}
 
-function signMessage(message, secret) {
-  var sessionMsg = Object.keys(message).map(function(val) {
-    if (typeof message[val] === 'object') {
-      return Object.keys(message[val]).map(function(val1) {
-        return val + '[' + val1 + ']=' + message[val][val1];
-      }).sort().join('&');
-    } else {
-      return val + '=' + message[val];
+  return AuthProxy;
+
+  /* Private
+  ---------------------------------------------------------------------- */
+  function generateAuthMsg(params) {
+    var message = {
+      application_id: config.creds.appId,
+      auth_key: config.creds.authKey,
+      nonce: Utils.randomNonce(),
+      timestamp: Utils.unixTime()
+    };
+    
+    // With user authorization
+    if (params.login && params.password) {
+      message.user = {login: params.login, password: params.password};
+    } else if (params.email && params.password) {
+      message.user = {email: params.email, password: params.password};
+    } else if (params.provider) {
+      // Via social networking provider (e.g. facebook, twitter etc.)
+      message.provider = params.provider;
+      if (params.scope) {
+        message.scope = params.scope;
+      }
+      if (params.keys && params.keys.token) {
+        message.keys = {token: params.keys.token};
+      }
+      if (params.keys && params.keys.secret) {
+        messages.keys.secret = params.keys.secret;
+      }
     }
-  }).sort().join('&');
-  
-  return crypto(sessionMsg, secret).toString();
-}
+    
+    return message;
+  }
+
+  function signMessage(message, secret) {
+    var sessionMsg = Object.keys(message).map(function(val) {
+      if (typeof message[val] === 'object') {
+        return Object.keys(message[val]).map(function(val1) {
+          return val + '[' + val1 + ']=' + message[val][val1];
+        }).sort().join('&');
+      } else {
+        return val + '=' + message[val];
+      }
+    }).sort().join('&');
+    
+    return crypto(sessionMsg, secret).toString();
+  }
+
+});
