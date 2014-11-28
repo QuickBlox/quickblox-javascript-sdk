6331fb04ee4e9e09727cb036801a05dd1b523840
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-11-28 11:58:48 +0200

    [browserify] qbStrophe.js

--- js/qbStrophe.js
+++ js/qbStrophe.js
@@ -1,4 +1,3 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
 /*
  * QuickBlox JavaScript SDK
  *
@@ -6,24 +5,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module) }
  *
  */
 
-define(['qbConfig', 'strophe'],
-function(config, Strophe) {
+require('../lib/strophe/strophe.min');
+var config = require('./qbConfig');
 
-  function Connection() {
-    var protocol = config.chatProtocol.active === 1 ? config.chatProtocol.bosh : config.chatProtocol.websocket;
-    var conn = new Strophe.Connection(protocol);
-    // if (config.debug) {
-      if (config.chatProtocol.active === 1) {
-        conn.xmlInput = function(data) { if (data.childNodes[0]) {for (var i = 0, len = data.childNodes.length; i < len; i++) { console.log('[QBChat RECV]:', data.childNodes[i]); }} };
-        conn.xmlOutput = function(data) { if (data.childNodes[0]) {for (var i = 0, len = data.childNodes.length; i < len; i++) { console.log('[QBChat SENT]:', data.childNodes[i]); }} };
-      } else {
-        conn.xmlInput = function(data) { console.log('[QBChat RECV]:', data); };
-        conn.xmlOutput = function(data) { console.log('[QBChat SENT]:', data); };
-      }
-    // }
+function Connection() {
+  var protocol = config.chatProtocol.active === 1 ? config.chatProtocol.bosh : config.chatProtocol.websocket;
+  var conn = new Strophe.Connection(protocol);
+  // if (config.debug) {
+    if (config.chatProtocol.active === 1) {
+      conn.xmlInput = function(data) { if (data.childNodes[0]) {for (var i = 0, len = data.childNodes.length; i < len; i++) { console.log('[QBChat RECV]:', data.childNodes[i]); }} };
+      conn.xmlOutput = function(data) { if (data.childNodes[0]) {for (var i = 0, len = data.childNodes.length; i < len; i++) { console.log('[QBChat SENT]:', data.childNodes[i]); }} };
+    } else {
+      conn.xmlInput = function(data) { console.log('[QBChat RECV]:', data); };
+      conn.xmlOutput = function(data) { console.log('[QBChat SENT]:', data); };
+    }
+  // }
 
-    return conn;
-  }
-  
-  return Connection;
-});
+  return conn;
+}
+
+module.exports = Connection;
