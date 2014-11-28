be543079a40f455b9b554452ee5714b35eb744ea
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-11-28 12:10:28 +0200

    [browserify] qbMain.js

--- js/qbMain.js
+++ js/qbMain.js
@@ -1,4 +1,3 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
 /*
  * QuickBlox JavaScript SDK
  *
@@ -6,81 +5,72 @@ if (typeof define !== 'function') { var define = require('amdefine')(module) }
  *
  */
 
-define(function(require) {
-  var config = require('qbConfig'),
-      Proxy = require('qbProxy'),
-      Connection = require('qbStrophe'),
-      Auth = require('qbAuth'),
-      Users = require('qbUsers'),
-      Chat = require('qbChat'),
-      WebRTC = require('qbWebRTC'),
-      Content = require('qbContent'),
-      Location = require('qbLocation'),
-      Messages = require('qbMessages'),
-      Data = require('qbData');
+var config = require('./qbConfig');
 
-  // Actual QuickBlox API starts here
-  function QuickBlox() {}
+// Actual QuickBlox API starts here
+function QuickBlox() {}
 
-  QuickBlox.prototype = {
+QuickBlox.prototype = {
 
-    init: function(appId, authKey, authSecret, debug) {
-      if (debug && typeof debug === 'boolean') config.debug = debug;
-      else if (debug && typeof debug === 'object') config.set(debug);
+  init: function(appId, authKey, authSecret, debug) {
+    if (debug && typeof debug === 'boolean') config.debug = debug;
+    else if (debug && typeof debug === 'object') config.set(debug);
 
-      // create Strophe Connection object
-      var conn = new Connection();
-      
-      this.service = new Proxy();
-      this.auth = new Auth(this.service);
-      this.users = new Users(this.service);
-      this.chat = new Chat(this.service, conn);
-      this.webrtc = new WebRTC(this.service, conn);
-      this.content = new Content(this.service);
-      this.location = new Location(this.service);
-      this.messages = new Messages(this.service);
-      this.data = new Data(this.service);
-      
-      // Initialization by outside token
-      if (typeof appId === 'string' && !authKey && !authSecret) {
-        this.service.setSession({ token: appId });
-      } else {
-        config.creds.appId = appId;
-        config.creds.authKey = authKey;
-        config.creds.authSecret = authSecret;
-      }
-      if(console && config.debug) console.log('QuickBlox.init', this);
-    },
+    // include dependencies
+    var Proxy = require('./qbProxy'),
+        Connection = require('./qbStrophe'),
+        Auth = require('./modules/qbAuth'),
+        Users = require('./modules/qbUsers'),
+        Chat = require('./modules/qbChat'),
+        WebRTC = require('./modules/qbWebRTC'),
+        Content = require('./modules/qbContent'),
+        Location = require('./modules/qbLocation'),
+        Messages = require('./modules/qbMessages'),
+        Data = require('./modules/qbData');
 
-    createSession: function(params, callback) {
-      this.auth.createSession(params, callback);
-    },
-
-    destroySession: function(callback) {
-      this.auth.destroySession(callback);
-    },
-
-    login: function(params, callback) {
-      this.auth.login(params, callback);
-    },
-
-    logout: function(callback) {
-      this.auth.logout(callback);
-    }
+    // create Strophe Connection object
+    var conn = new Connection();
     
-  };
+    this.service = new Proxy();
+    this.auth = new Auth(this.service);
+    this.users = new Users(this.service);
+    this.chat = new Chat(this.service, conn);
+    this.webrtc = new WebRTC(this.service, conn);
+    this.content = new Content(this.service);
+    this.location = new Location(this.service);
+    this.messages = new Messages(this.service);
+    this.data = new Data(this.service);
+    
+    // Initialization by outside token
+    if (typeof appId === 'string' && !authKey && !authSecret) {
+      this.service.setSession({ token: appId });
+    } else {
+      config.creds.appId = appId;
+      config.creds.authKey = authKey;
+      config.creds.authSecret = authSecret;
+    }
+    if(console && config.debug) console.log('QuickBlox.init', this);
+  },
 
-  var QB = new QuickBlox();
+  createSession: function(params, callback) {
+    this.auth.createSession(params, callback);
+  },
 
-  // Node.js exports
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = QB;
-    module.exports.QuickBlox = QuickBlox;
+  destroySession: function(callback) {
+    this.auth.destroySession(callback);
+  },
 
-  // Window scoped variable (QB) for using in browsers
-  } else {
-    window.QB = QB;
-    return QB;
+  login: function(params, callback) {
+    this.auth.login(params, callback);
+  },
+
+  logout: function(callback) {
+    this.auth.logout(callback);
   }
+  
+};
 
-});
+var QB = new QuickBlox();
+QB.QuickBlox = QuickBlox;
+
+module.exports = QB;
