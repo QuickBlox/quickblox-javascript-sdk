40404974686f0f779a73aee035ff684cf8d00527
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2015-01-26 19:32:33 +0200

    test commit

---
+++ samples/config.js
@@ -0,0 +1,22 @@
+var CONFIG = {
+  // QuickBlox Application credentials
+  QBApp: {
+    appId: 8445,
+    authKey: 'yyRRFJMjRqYVdgT',
+    authSecret: 'zRmYWAr5djh48fu'
+  },
+
+  user1 = {
+    id: 2233911,
+    login: 'user1',
+    password: '123123123',
+    full_name: 'User1'
+  },
+
+  user2 = {
+    id: 2233912,
+    login: 'user2',
+    password: '123123123',
+    full_name: 'User2'
+  }
+};
