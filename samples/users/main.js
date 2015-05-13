e4cd714f2bb0a594fe909105a954c8cd5878ccd6
Igor Khomenko <igor@quickblox.com>
2015-05-13 16:42:48 +0300

    1st working version of Users sample

--- samples/users/main.js
+++ samples/users/main.js
@@ -1,4 +1,6 @@
 
+// Init QuickBlox application here
+//
 QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret);
 
 $(document).ready(function() {
@@ -10,6 +12,9 @@ $(document).ready(function() {
     console.log('Session create callback', err, result);
   });
 
+
+  // Create user
+  //
   $('#sign_up').on('click', function() {
     var login = $('#usr_sgn_p_lgn').val();
     var password = $('#usr_sgn_p_pwd').val();
@@ -20,24 +25,50 @@ $(document).ready(function() {
 
     QB.users.create(params, function(err, user){
       if (user) {
-        $('#output_place').html(JSON.stringify(user));
+        $('#output_place').val(JSON.stringify(user));
       } else  {
-        $('#output_place').html(JSON.stringify(err));
+        $('#output_place').val(JSON.stringify(err));
       }
 
       $("#progressModal").modal("hide");
     });
   });
 
+
+  // Login user
+  //
   $('#sign_in').on('click', function() {
     var login = $('#usr_sgn_n_lgn').val();
     var password = $('#usr_sgn_n_pwd').val();
 
     console.log("login: " + login + ", password: " + password);
+
+    var params = { 'login': login, 'password': password};
+
+    QB.login(params, function(err, user){
+      if (user) {
+        $('#output_place').val(JSON.stringify(user));
+      } else  {
+        $('#output_place').val(JSON.stringify(err));
+      }
+
+      $("#progressModal").modal("hide");
+    });
   });
 
+
+  // Logout user
+  //
   $('#sign_out').on('click', function() {
- 
+     QB.logout(function(err, result){
+      if (result) {
+        $('#output_place').val(JSON.stringify(result));
+      } else  {
+        $('#output_place').val(JSON.stringify(err));
+      }
+
+      $("#progressModal").modal("hide");
+    });
   });
 
 });
