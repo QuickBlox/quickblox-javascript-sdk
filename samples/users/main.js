e97fbbcc1c713d4690e36e24e5f0a7ff8ecaab6e
Igor Khomenko <igor@quickblox.com>
2015-05-13 13:17:50 +0300

    finished Users sign in

--- samples/users/main.js
+++ samples/users/main.js
@@ -3,11 +3,30 @@ QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret);
 
 $(document).ready(function() {
 
+  // First of all create a session and obtain a session token
+  // Then you will be able to run requests to Users
+  //
+  QB.createSession(function(err,result){
+    console.log('Session create callback', err, result);
+  });
+
   $('#sign_up').on('click', function() {
     var login = $('#usr_sgn_p_lgn').val();
     var password = $('#usr_sgn_p_pwd').val();
 
     console.log("login: " + login + ", password: " + password);
+
+    var params = { 'login': login, 'password': password};
+
+    QB.users.create(params, function(err, user){
+      if (user) {
+        $('#output_place').html(JSON.stringify(user));
+      } else  {
+        $('#output_place').html(JSON.stringify(err));
+      }
+
+      $("#progressModal").modal("hide");
+    });
   });
 
   $('#sign_in').on('click', function() {
