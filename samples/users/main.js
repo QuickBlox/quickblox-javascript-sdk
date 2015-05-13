d8b8a2b68bc310ee0c933a0e517268b85c0738bf
Igor Khomenko <igor@quickblox.com>
2015-05-13 18:50:49 +0300

    finished 'get users'; added 'User Update'

--- samples/users/main.js
+++ samples/users/main.js
@@ -76,20 +76,65 @@ $(document).ready(function() {
 
     var params;
 
-    var request_for_many_user = true
+    var request_for_many_user = false
 
     switch (filter_type) {
       // all users, no filters<
       case "1":
         params = { page: '1', per_page: '100'};
+        request_for_many_user = true
         break;
 
       // by id
       case "2":
         params = parseInt(filter_value);
-        request_for_many_user = false
+        break;
+
+      // by login
+      case "3":
+        params = {login: filter_value};
+        break;
+
+      // by fullname
+      case "4":
+        params = {full_name: filter_value};
+        break;
+
+      // by facebook id
+      case "5":
+        params = {facebook_id: filter_value};
+        break;
+
+      // by twitter id
+      case "6":
+        params = {twitter_id: filter_value};
+        break;
+
+      // by email
+      case "7":
+        params = {email: filter_value};
+        break;
+
+      // by tags
+      case "8":
+        params = {tags: filter_value};
+        break;
+
+      // by external id
+      case "9":
+        params = {external: filter_value};
+        break;
+
+      // custom filters
+      case "10":
+        // More info about filters here 
+        // http://quickblox.com/developers/Users#Filters
+        params = {filter: { field: 'email', param: 'eq', value: 'nobody@nowhere.org' }};
+        request_for_many_user = true
         break;
     }
+
+    console.log("filter_value: " + filter_value)
 
     if(request_for_many_user){
       QB.users.listUsers(params, function(err, user){
@@ -112,6 +157,23 @@ $(document).ready(function() {
         $("#progressModal").modal("hide");
       });
     }
+  });
+
+  // Update user
+  //
+  $('#update').on('click', function() {
+    var user_id = $('#usr_upd_id').val();
+    var user_fullname = $('#usr_upd_full_name').val();
+
+    QB.users.update(parseInt(user_id), {full_name: user_fullname}, function(err, user){
+      if (user) {
+        $('#output_place').val(JSON.stringify(user));
+      } else  {
+        $('#output_place').val(JSON.stringify(err));
+      }
+
+      $("#progressModal").modal("hide");
+    });
   });
 
 });
