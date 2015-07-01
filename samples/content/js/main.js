aac3eee3f615283465d6b3c619fd21c78db97582
Vladlukhanin <vladlukhanin@gmail.com>
2015-07-01 18:42:29 +0300

    initial version of content sample

--- samples/content/js/main.js
+++ samples/content/js/main.js
@@ -1,4 +1,7 @@
 QB.init(24507, 'eskKgMVUc7nRSbq', 'UW6VdHKdAV8Jxc7',true);
+
+var user_id;
+
 $(document).ready(function(){
 	QB.createSession({login: 'vladlukhanin', password: 'papajad24'}, function(err, result){
 	   if (err) { 
@@ -6,12 +9,12 @@ $(document).ready(function(){
 	    } else {
 	        console.log('Session created with id ' + result.id);
 	 		var token = result.token;
+	 			user_id = result.id;
 
 	 		// get content images
 	        QB.content.list({page:'1', per_page: '6'}, function(error, response){
 	    	  	$.each(response.items, function(index, item){
 	    	  		var cur = this.blob;
-    					// response.items.length = response.total_entries;
 	    	  		$('#pictures').append("<img src='http://api.quickblox.com/blobs/"+cur.id+"/download.xml?token="+token+"' alt='"+cur.name+"' class='animals img-responsive col-md-6 col-sm-6 col-xs-6' />");
 	    	  	}); 
 				  if (error) { 
@@ -20,17 +23,32 @@ $(document).ready(function(){
 		    	  	console.log(response);	    	  	
 		    	  }
 
-				});
+			});
+
+
+			var files = $("input[type=file]")[0].files;
+
+			QB.content.create({name: "Test file", content_type: "image/jpeg", 'public': true}, function(err, response){
+				if (err) {
+			        console.log(err);
+			    } else {
+			        // Response will contain blob ID.
+
+			        var data = {
+			            blob_id: response.id,
+			            blob_owner: user_id
+			        }
+
+			    }
+			});
 	    }
     });
 
-		// $(window).scroll( function (e) {
-		// 	var need_height = $(window).height();
-		// 	if ($(document).scrollTop() > need_height) {
-		//         alert('Элемент foo был прокручен... скроллирован... ну как там это называется то?!');
-		// 	}
-		// });
+		$(window).scroll(function() {
+		     if  ($(window).scrollTop() == $(document).height() - $(window).height()) 
+		     {
+		        alert("это сработало событие");
+		     }
+		});
 	
 });
-
-
