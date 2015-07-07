38b0ecb40e10b2a4994541d98c0fa64f150fc1a1
Vladlukhanin <vladlukhanin@gmail.com>
2015-07-07 16:01:39 +0300

    finished version of content sample

--- samples/content/js/main.js
+++ samples/content/js/main.js
@@ -1,59 +1,26 @@
-QB.init(24507, 'eskKgMVUc7nRSbq', 'UW6VdHKdAV8Jxc7', true);
+QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, true);
 
 $(document).ready(function(){
-// create session
-QB.createSession({login: 'vladlukhanin', password: 'papajad24'}, function(err, result) {
+
+QB.createSession(QBUser, function(err, result) {
+	var token = result.token;
+		user_id = result.id;
+		uploadPages = 0;
+		filesCount = 0;
+		finished = false;
+
 	if (err) { 
 		console.log('Something went wrong: ' + err);
 	} else {
 		console.log('Session created with id ' + result.id);
 
-		var token = result.token;
-			user_id = result.id;
-			uploadPages = 0;
-			filesCount = 0;
-			finished = false;
-		// get content files
-		function uploadingFiles() {
-			if (finished != true) {
-				$("#loadwnd").show(0);
-				uploadPages = uploadPages + 1;
-		
-				QB.content.list({page: uploadPages, per_page: '9'}, function(error, response) {
-				$.each(response.items, function(index, item){
-					var cur = this.blob;
-
-					insertImage(cur.id, cur.name, token, true);
-				});
-					if (error) {
-						console.log(error);
-					} else {
-						console.log(response);
-						$("#loadwnd").delay(1000).fadeOut(1000);
-					}
-					
-					var totalEntries = response.total_entries;
-						entries = response.items.length;
-						filesCount = filesCount + entries;
-
-					if (filesCount >= totalEntries) {
-						finished = true;
-					}
-						console.log(totalEntries);
-						console.log(response.items.length);
-						console.log(filesCount);
-						console.log(finished);
-				});
-			}
-		}
-
-		uploadingFiles();
+		uploadingFiles(token);
 
 		// uploading files scroll event
 		$(window).scroll(function() {
 			if  ($(window).scrollTop() == $(document).height() - $(window).height())
 			{
-				uploadingFiles();
+				uploadingFiles(token);
 			}
 		});
 
@@ -84,12 +51,42 @@ QB.createSession({login: 'vladlukhanin', password: 'papajad24'}, function(err, r
 	}
 });
 });
-
+// add images
 function insertImage(fileId, fileName, token, toAppend){
 	var imageHTML = "<img src='http://api.quickblox.com/blobs/"+fileId+"/download.xml?token="+token+"' alt='"+fileName+"' class='animals img-responsive col-md-4 col-sm-6 col-xs-12' />";
-	if(toAppend){
+	if (toAppend) {
 		$('#pictures').append(imageHTML);
-	}else{
+		console.log(token);
+	} else {
 		$('#pictures').prepend(imageHTML);
+	}
+}
+// get content files
+function uploadingFiles(token) {
+	if (finished != true) {
+		$("#loadwnd").show(0);
+		uploadPages = uploadPages + 1;
+
+		QB.content.list({page: uploadPages, per_page: '9'}, function(error, response) {
+		$.each(response.items, function(index, item){
+			var cur = this.blob;
+
+			insertImage(cur.id, cur.name, token, true);
+		});
+			if (error) {
+				console.log(error);
+			} else {
+				console.log(response);
+				$("#loadwnd").delay(1000).fadeOut(1000);
+			}
+			
+			var totalEntries = response.total_entries;
+				entries = response.items.length;
+				filesCount = filesCount + entries;
+
+			if (filesCount >= totalEntries) {
+				finished = true;
+			}
+		});
 	}
 }
