00dc23476b26ac41c9e243c750fecd1230de8a15
Vladlukhanin <vladlukhanin@gmail.com>
2015-07-08 13:31:47 +0300

    initial version of customobjects v.2

--- samples/castomobjects/js/main.js
+++ samples/castomobjects/js/main.js
@@ -1,13 +1,37 @@
 QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, true);
 
-$(document).ready(function(){
+// Create session
+QB.createSession(QBUser, function(err, result){
+   if (err) { 
+        console.log('Something went wrong: ' + err);
+    } else {
+        console.log('Session created with id ' + result.id);
+ 
+        // Get all posts
+        QB.data.list("Post", null, function(err, result){
+            if (err) { 
+                console.log('Something went wrong: ' + err);
+            } else {
+                // render posts here
+                var containerElement = document.getElementById('posts-container');
+                for (var i=0; i < result.items.length; i++) {
+                    var postElement = document.createElement('div'); 
+                    postElement.className = 'starter-template';
+ 
+                    var postTitle = document.createElement('h1'); 
+                    postTitle.innerHTML = '#' + (i+1) + ' ' + result.items[result.items.length-i-1].title;
+                    postElement.appendChild(postTitle);
+ 
+                    var postBody = document.createElement('p'); 
+                    postBody.className = 'lead';
+                    postBody.innerHTML = result.items[result.items.length-i-1].body;
+                    postElement.appendChild(postBody);
+ 
+                    containerElement.appendChild(postElement);
+                }
+            }
+        });
+    }
+});
 
-	QB.createSession(QBUser, function(err, result) {
-		if (err) { 
-			console.log('Something went wrong: ' + err);
-		} else {
-			console.log('Session created with id ' + result.id);
-		}
-	});
-	
 });
