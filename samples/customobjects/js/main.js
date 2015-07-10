78fafe4750048b1fe8d66bd38e3934f36df369b9
Vladlukhanin <vladlukhanin@gmail.com>
2015-07-10 12:08:46 +0300

    almost ready version ( contains errors )

--- samples/customobjects/js/main.js
+++ samples/customobjects/js/main.js
@@ -1,29 +1,81 @@
 QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, true);
+
 // Create session
+var filter = {sort_asc: 'created_at'};
+
 QB.createSession(QBUser, function(err, result){
 	if (err) {
 		console.log('Something went wrong: ' + err);
 	} else {
 		console.log('Session created with id ' + result.id);
 		// Get all posts
-		QB.data.list("Blog", null, function(err, result){
-			if (err) { 
-				console.log('Something went wrong: ' + err);
-			} else {
-				// render posts here
-				var containerElement = $('#posts-container');
-				for (var i=0; i < result.items.length; i++) {
+		getAllPosts();
+	}
+		$('#send_post').click(function(e) {
+			e.preventDefault(); 
+			var textTitle = $('#title_post')[0].value;
+				textBody = $('#body_post')[0].value;
+
+			if (textTitle && textBody) {
+				$("#load-img").show(0);
+			}
+				addNewPost(textTitle, textBody);
+		});
+});
+
+function getAllPosts() {
+
+	QB.data.list("Blog", filter, function(err, result){
+		if (err) { 
+			console.log('Something went wrong: ' + err);
+		} else {
+			// render posts here
+			console.log(result);
+			var containerElement = $('#posts-container');
+			for (var i=0; i < result.items.length; i++) {
+				var postElement = $('<div></div>').addClass('starter-template');
+
+				var postTitle = $('<h1></h1>').html('#' + (i+1) + ' ' + result.items[result.items.length-i-1].title);
+				postElement.append(postTitle);
+
+				var postBody = $('<p></p>').addClass('lead').html(result.items[result.items.length-i-1].body);
+				postElement.append(postBody);
+
+				containerElement.append(postElement);
+				addNewPost(i);
+			}	
+		}
+	});
+}
+
+function addNewPost(textTitle, textBody, i) {
+	QB.data.create("Blog", {title: textTitle, body: textBody}, function(err, res){
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(res);
+			$("#load-img").delay(1000).fadeOut(1000);
+			$('#myModal').modal('hide');
+
+			QB.data.list("Blog", filter, function(err, result){
+				if (err) { 
+					console.log(err);
+				} else {
+					console.log(result);
+
+					var containerElement = $('#posts-container');
+
 					var postElement = $('<div></div>').addClass('starter-template');
 
-					var postTitle = $('<h1></h1>').html('#' + (i+1) + ' ' + result.items[result.items.length-i-1].title);
+					var postTitle = $('<h1></h1>').html('#' + (i+1) + ' ' + textTitle);
 					postElement.append(postTitle);
 
-					var postBody = $('<p></p>').addClass('lead').html(result.items[result.items.length-i-1].body);
+					var postBody = $('<p></p>').addClass('lead').html(textBody);
 					postElement.append(postBody);
 
 					containerElement.append(postElement);
-				}
-			}
-		});
-	}
-});
+					}
+			});
+		}
+	});
+}
