329679f8d9b29e98110edfa2550a765ed1e9ca11
Igor Khomenko <igor@quickblox.com>
2015-07-12 11:34:19 +0300

    back to chat sample 1.0

--- samples/chat/main.js
+++ samples/chat/main.js
@@ -3,11 +3,7 @@
 //
 QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret);
 
-var dialogs = {};
-var users = {};
-
-var currentDialog;
-var currentUser;
+var opponent, currentUser;
 
 $(document).ready(function() {
 
@@ -17,15 +13,26 @@ $(document).ready(function() {
   // User1 login action
   //
   $('#user1').click(function() {
-    currentUser = QBUser1;
+    opponent = QBUser2;
     connectChat(QBUser1);
   });
 
   // User2 login action
   //
   $('#user2').click(function() {
-    currentUser = QBUser2;
+    opponent = QBUser1;
     connectChat(QBUser2);
+  });
+
+  // Send message action
+  //
+  $('#sendMessage').click(function() {
+    var msg = $('#message').val().trim();
+    $('#message').val('').focus();
+
+    if (msg.length > 0){
+      sendMessage(opponent.id, msg);
+    }
   });
 });
 
@@ -38,7 +45,9 @@ function connectChat(user) {
   //
   QB.createSession({login: user.login, password: user.pass}, function(err, res) {
     if (res) {
-      QB.chat.connect({userId: user.id, password: user.pass}, function(err, roster) {
+      var user_jid = QB.chat.helpers.getUserJid(user.id, QBApp.appId);
+      QB.chat.connect({jid: user_jid, password: user.pass}, function(err, roster) {
+        $("#loginForm").modal("hide");
 
         if (err) {
           console.log(err);
@@ -47,171 +56,49 @@ function connectChat(user) {
 
           QB.chat.onMessageListener = showMessage;
 
-          // Load chat dialogs
-          //
-          QB.chat.dialog.list(null, function(err, resDialogs) {
-
-            if (err) {
-
-            } else {
-
-              var occupantsIds = [];  
-
-              // repackage dialogs data
-              //
-              resDialogs.items.forEach(function(item, i, arr) {
-                var dialogId = item._id;
-                dialogs[dialogId] = item;
-
-                // collect all occupants ids
-                item.occupants_ids.map(function(userId) {
-                  occupantsIds.push(userId);
-                });
-              });
-
-
-              // Load all dialogs' users
-              //
-              var params = {filter: { field: 'id', param: 'in', value: jQuery.unique(occupantsIds) }};
-              QB.users.listUsers(params, function(err, result){
-                if (result) {
-                  result.items.forEach(function(item, i, arr) {
-                    users[item.user.id] = item.user;
-                  });
-
-
-                  // show dialogs
-                  //
-                  resDialogs.items.forEach(function(item, i, arr) {
-                    var dialogId = item._id;
-                    var dialogName = item.name;
-                    var dialogLastMessage = item.last_message;
-                    var dialogUnreadMessagesCount = item.unread_messages_count; 
-
-                    var dialogHtml = '<a href="#" class="list-group-item" onclick="triggerDialog(this, ' + "'" + dialogId + "'" + ')">' + 
-                      '<span class="badge">' + dialogUnreadMessagesCount + '</span>' + 
-                      '<h4 class="list-group-item-heading">' + dialogName + '</h4>' + 
-                      '<p class="list-group-item-text">' + (dialogLastMessage === null ?  "" : dialogLastMessage) + '</p>' + 
-                      '</a>';
-
-                    $('#dialogs-list').append(dialogHtml);
-                  });
-
-                  // trigger 1st dialog
-                  triggerDialog($('#dialogs-list').children()[0], resDialogs.items[0]._id);
-
-                } else  {
-                  
-                }
-
-                $("#loginForm").modal("hide");
-              });
-            }
-          });
+          $('#feed').val('User : CONNECTED at ' + getLocalTime());
         }
       });
     }
   });
 }
 
-function triggerDialog(element, dialogId){
-
-  // deselect
-  var kids = $( "#dialogs-list" ).children();
-  kids.removeClass("active");
-
-  // select
-  element.className = element.className + " active";
-
-  currentDialog = dialogs[dialogId];
-
-  // Load messages history
-  //
-  var params = {chat_dialog_id: dialogId, sort_asc: 'date_sent', limit: 100, skip: 0};
-  QB.chat.message.list(params, function(err, messages) {
-    $('#messages-list').html('');
-
-    if (messages) {
-      if(messages.items.length == 0){
-        $("#no-messages-label").removeClass('hide');
-      }else{
-        $("#no-messages-label").addClass('hide');
-        
-        messages.items.forEach(function(item, i, arr) {
-          var messageText = item.message;
-          var messageSenderId = item.sender_id;
-          var messageDateSent = new Date(item.date_sent*1000);
-
-          console.log(item.read_ids);
-
-          var messageHtml = buildMessageHTML(messageText, messageSenderId, messageDateSent);
-
-          $('#messages-list').append(messageHtml);
-        });
-      }
-    }else{
-    
-    }
-  });
-}
-
-function clickSendMessage(){
-  var currentText = $('#message_text').val().trim();
-  $('#message_text').val('').focus();
- 
-  if (currentText.length == 0){
-    return;
-  }
-
-  // send a message
-  //
+// Send a chat message
+//
+function sendMessage(user_id, val) {
   var msg = {
-    type: currentDialog.type == 3 ? 'chat' : 'groupchat',
-    body: currentText,
+    type: 'chat',
+    body: val,
+
     extension: {
-      save_to_history: 1,
-    },
-    senderId: currentUser.id
+      time: Math.floor(Date.now() / 1000),
+    }
   };
-  //
-  if(currentDialog.type == 3){
-    console.log(currentDialog.occupants_ids);
 
-    var userId = getRecipientId(currentDialog.occupants_ids, currentUser.id);
+  var user_jid = QB.chat.helpers.getUserJid(user_id, QBApp.appId);
+  QB.chat.send(user_jid, msg);
 
-    QB.chat.send(userId, msg);
-  }else{
-    QB.chat.send(currentDialog.xmpp_room_jid, msg);
-  }
-
-  showMessage(msg);
+  showMessage(null, msg);
 }
 
 // Show messages in UI
 //
-function showMessage(msg) {
-  // add a message to list
-  var messageHtml = buildMessageHTML(msg.body, msg.senderId, new Date());
-  $('#messages-list').append(messageHtml);
+function showMessage(userId, msg) {
 
-  // scroll to bottom
-  var mydiv = $('#messages-list');
-  mydiv.scrollTop(mydiv.prop('scrollHeight'));
+  var body = msg.body,
+      time = msg.extension && msg.extension.time,
+      messageDate = new Date(time * 1000);
+
+  var message = "\n";
+      message += (userId === null ? "Me" : "Opponent");
+      message += " (" + messageDate.getHours() + ':' + (messageDate.getMinutes().toString().length === 1 ? '0'+messageDate.getMinutes() : messageDate.getMinutes()) + ':' + messageDate.getSeconds() + ")";
+      message +=  ": ";
+      message += body;
+
+  var currentText = $('#feed').val();
+  $('#feed').val(currentText + message); 
 }
 
-function buildMessageHTML(messageText, messageSenderId, messageDateSent){
-  var messageHtml = '<div class="list-group-item">' + 
-                    '<time datetime="' + messageDateSent + '" class="pull-right">' + jQuery.timeago(messageDateSent) + '</time>' + 
-                    '<h4 class="list-group-item-heading">' + 'Igor Khomenko' + '</h4>' + 
-                    '<p class="list-group-item-text">' + messageText + '</p>' + 
-                    '</div>';
-  return messageHtml;
-}
-
-function getRecipientId(occupantsIds, currentUserId){
-  occupantsIds.forEach(function(item, i, arr) {
-    if(item != currentUserId){
-      return item;
-    }  
-  });
+function getLocalTime() {
+  return (new Date).toTimeString().split(' ')[0];
 }
