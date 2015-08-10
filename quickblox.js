69eb2afa1bbfe7c3d351165fb8443d3291114b72
Igor Khomenko <igor@quickblox.com>
2015-08-10 15:17:45 +0300

    built new quickblox.js

--- quickblox.js
+++ quickblox.js
@@ -1,4 +1,4 @@
-/* QuickBlox JavaScript SDK - v1.9.3.1 - 2015-08-06 */
+/* QuickBlox JavaScript SDK - v1.10.0 - 2015-08-10 */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.QB = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*
@@ -959,7 +959,6 @@ Helpers.prototype = {
 
   jidOrUserId: function(jid_or_user_id) {
     var jid;
-    console.log(jid_or_user_id);
     if (typeof jid_or_user_id === 'string') {
       jid = jid_or_user_id;
     } else if (typeof jid_or_user_id === 'number') {
@@ -2386,7 +2385,7 @@ Blob.prototype.download = function() {
  */
 
 var config = {
-  version: '1.9.3.1',
+  version: '1.10.0',
   creds: {
     appId: '',
     authKey: '',
