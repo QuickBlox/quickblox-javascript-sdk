f55d86ab287eb3e8fe29466b8c3f3205d20d1019
vladlukhanin <vladlukhanin@gmail.com>
2015-09-14 17:55:43 +0300

    added roster test

--- samples/chat/js/connection.js
+++ samples/chat/js/connection.js
@@ -1,6 +1,7 @@
 //"use strict";
 
 var currentUser;
+var rosterJid;
 
 $(document).ready(function() {
 
@@ -40,6 +41,7 @@ function connectToChat(user) {
         if (err) {
           console.log(err);
         } else {
+          console.log(roster);
           // setup message listeners
           //
           setupOnMessageListener();
@@ -68,3 +70,56 @@ $(document).ready(
         $(".nice-scroll").niceScroll({cursorcolor:"#02B923", cursorwidth:"7", zindex:"99999"});
     }
 );
+
+
+function showRosterPopup() {
+  $("#rosterForm").modal("show");
+  $('#rosterForm .progress').hide();
+
+  if (currentUser == QBUser1) {
+    rosterJid = QB.chat.helpers.getUserJid(QBUser2.id, QBApp.appId);
+  } else {
+    rosterJid = QB.chat.helpers.getUserJid(QBUser1.id, QBApp.appId);
+  }
+}
+
+function rosterAdd() {
+  QB.chat.roster.add(rosterJid, function() {
+    console.log(rosterJid);
+  });
+}
+
+function rosterRemove() {
+  QB.chat.roster.remove(rosterJid, function() {
+    console.log(rosterJid);
+  });
+}
+
+function rosterConfirm() {
+  QB.chat.roster.confirm(rosterJid, function() {
+    console.log(rosterJid);
+  });
+}
+
+function rosterReject() {
+  QB.chat.roster.reject(rosterJid, function() {
+    console.log(rosterJid);
+  });
+}
+
+QB.chat.onSubscribeListener = function(userId) {
+  console.log('onSubscribeListener');
+};
+
+QB.chat.onConfirmSubscribeListener = function(userId) {
+  console.log('onConfirmSubscribeListener');
+};
+
+QB.chat.onRejectSubscribeListener = function(userId) {
+  console.log('onRejectSubscribeListener');
+};
+
+QB.chat.onContactListListener = function(userId, type) {
+  console.log('onContactListListener');
+  console.log('status: '+type);
+};
--- samples/content/js/config.js
+++ samples/content/js/config.js
@@ -1,67 +1,28 @@
-// // entrada
-// var CONFIG = {
-//     endpoints: {
-//       api: 'apientrada.quickblox.com',
-//       chat: 'chatentrada.quickblox.com',
-//       muc: 'muc.chatentrada.quickblox.com',
-//       turn: 'turnserver.quickblox.com',
-//       s3Bucket: 'atlas-storage-entrada'
-//     },
-//     chatProtocol: {
-//       bosh: 'http://chatentrada.quickblox.com:5280',
-//       server: 'chatentrada.quickblox.com',
-//       websocket: 'ws://chatentrada.quickblox.com:5290',
-//       active: 2
-//     },
-//     ssl: true,
-//     debug: true
-//   }
-
-// var QBUser = {
-//       login: 'testcontent',
-//       pass: 'testcontent'
-//     };
-
-// QB.init(10, 'vnDPCeZQ4NskvhL', 'EF3n-Zj99WErTKR', CONFIG);
-
-// var QBApp = {
-//   appId: 6,
-//   authKey: 'ccBf5X8r3-jax3j',
-//   authSecret: '47sEGjyOPO65Ezr'
-// };
-
-// var QBUser = {
-//   login: "test",   
-//   password: "testtest" 
-// };
-
-// var QBConfig = {
-
-// }
-
-// QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, QBConfig);
-
-var CONFIG = {
+var config = {
     endpoints: {
-      api: 'apibsdenterprise.quickblox.com',
-      chat: 'chatbsdenterprise.quickblox.com',
-      muc: 'muc.chatbsdenterprise.quickblox.com',
-      turn: 'turnserver.quickblox.com',
-      s3Bucket: 'qb-bsdenterprise-s3'
+        api: "apivronetwork.quickblox.com"
+        chat: "chatvronetwork.quickblox.com"
+        s3Bucket: "qb-vronetwork"
+        turn: "turnserver.quickblox.com"
     },
     chatProtocol: {
-      bosh: 'http://chatbsdenterprise.quickblox.com:5280',
-      server: 'chatbsdenterprise.quickblox.com',
-      websocket: 'ws://chatbsdenterprise.quickblox.com:5291',
-      active: 1
+        bosh: "https://chatvronetwork.quickblox.com:5281",
+        websocket: "wss://chatvronetwork.quickblox.com:5291",
+        active: 2
     },
-    ssl: true,
-    debug: true
-  }
+    debug: true,
+    ssl: true
+};
+
+var QBApp = {
+  appId: 16381,
+  authKey: 'JmecSQ5e8XGnVxr',
+  authSecret: 'RGFhyTj4gMq4FKP'
+};
 
 var QBUser = {
-        login: 'quickuser',
-        pass: 'quickuser'
-    }
-    
-QB.init(2, 'p2M9xv2K5CurSab', '9PKGO2EPcqcey6x', CONFIG);
+  login: "Quickblox",   
+  password: "Quickblox" 
+};
+
+QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, QBConfig);
