cdb38dbd35aed92a83b7855188fde1d7c2e2c470
Igor Khomenko <igor@quickblox.com>
2015-09-30 13:15:10 +0300

    more users

--- samples/webrtc/config.js
+++ samples/webrtc/config.js
@@ -40,7 +40,28 @@ var QBUsers = [
     login: 'webuser113',
     password: 'webuser113',
     full_name: 'User 3',
-    colour: '51a209'
+    colour: '11a2a9'
+  },
+  {
+    id: 5719859,
+    login: 'webuser114',
+    password: 'webuser114',
+    full_name: 'User 4',
+    colour: '51c209'
+  },
+  {
+    id: 5719860,
+    login: 'webuser115',
+    password: 'webuser115',
+    full_name: 'User 5',
+    colour: '511209'
+  },
+  {
+    id: 5719866,
+    login: 'webuser116',
+    password: 'webuser116',
+    full_name: 'User 6',
+    colour: '01e209'
   }
 ];
 
