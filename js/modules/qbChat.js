1954db257d19f0e97fd60d15be0b5aa9224b066e
vladlukhanin <vladlukhanin@gmail.com>
2015-10-13 11:49:31 +0300

    autosend 'received' status

--- js/modules/qbChat.js
+++ js/modules/qbChat.js
@@ -138,6 +138,18 @@ function ChatProxy(service, webrtcModule, conn) {
       return true;
     }
 
+    // autosend 'received' status
+    if (markable) {
+      params = {
+        messageId: messageId,
+        userId: userId,
+        dialogId: dialogId
+      };
+
+      self.sendDeliveredStatus(params);
+      return true;
+    }
+
     // chat markers
     if (marker) {
       if (delivered) {
