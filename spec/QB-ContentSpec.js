c3a0136dc3ee561d78d35853f39d299fc2f4ce88
dimaspirit <dimaspirit@inbox.ru>
2016-01-19 17:54:24 +0200

    adapt ContentSpec

--- spec/QB-ContentSpec.js
+++ spec/QB-ContentSpec.js
@@ -3,6 +3,11 @@ describe('Content API', function() {
 
   var REST_REQUESTS_TIMEOUT = 3000;
 
+  var isNodeEnv = typeof window === 'undefined' && typeof exports === 'object';
+  var QB = isNodeEnv ? require('../js/qbMain') : window.QB;
+  var CREDENTIALS = isNodeEnv ? require('./config').CREDENTIALS : window.CREDENTIALS;
+  var QBUser1 = isNodeEnv ? require('./config').QBUser1 : window.QBUser1;
+
   var token,
       data = {},
       fileUID = '97f5802dcbd34a59a4921d73f6baedd000',
@@ -13,7 +18,7 @@ describe('Content API', function() {
 
     QB.createSession(QBUser1, function(err, session) {
       if (err) {
-        done.fail("Create session error: " + JSON.stringify(err));
+        done.fail('Create session error: ' + JSON.stringify(err));
       } else {
         expect(session).not.toBeNull();
         token = session.token;
@@ -24,14 +29,18 @@ describe('Content API', function() {
   }, REST_REQUESTS_TIMEOUT);
 
   it('can create and upload files', function(done) {
+    if(isNodeEnv) {
+      pending('Working on fix "new File" in Node env.');
+    }
+
     var d = new Date(2015, 10, 13, 14, 30, 30, 600),
-        genFile = new File(["Hello QuickBlox"], "QB.txt", {type: "text/plain", lastModified: d});
+        genFile = new File(['Hello QuickBlox'], 'QB.txt', {type: 'text/plain', lastModified: d});
 
     data = {name: genFile.name, file: genFile, type: genFile.type, size: genFile.size, public: false};
 
     QB.content.createAndUpload(data, function(err, res) {
       if (err) {
-        done.fail("Create and upload files error: " + JSON.stringify(err));
+        done.fail('Create and upload files error: ' + JSON.stringify(err));
       }else{
         expect(res).not.toBeNull();
         expect(res.name).toBe('QB.txt');
@@ -55,15 +64,19 @@ describe('Content API', function() {
   }, REST_REQUESTS_TIMEOUT);
 
   it('can delete content objects', function(done) {
+    if(isNodeEnv) {
+      pending('Working on fix "new File" in Node env.');
+    }
+
     QB.content.createAndUpload(data, function(err, response) {
       if (err) {
-        done.fail("Create and upload files error: " + JSON.stringify(err));
+        done.fail('Create and upload files error: ' + JSON.stringify(err));
       }else{
         var elemId = response.id;
 
         QB.content.delete(elemId, function(err, result) {
           if (err) {
-            done.fail("Delete content objects error: " + JSON.stringify(err));
+            done.fail('Delete content objects error: ' + JSON.stringify(err));
           }else{
             expect(result).toEqual(true);
 
