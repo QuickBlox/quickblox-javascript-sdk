a596c8fd132b4b0e1c8da3bd5acd99f845b7332f
Dmitriy Nezhidenko <dehimb@gmail.com>
2016-04-12 14:42:12 +0200

    Added stickers (#126)
    - added stickers by 908Inc to chat sample

--- samples/chat/js/config.js
+++ samples/chat/js/config.js
@@ -11,6 +11,20 @@ var config = {
   debug: {
     mode: 1,
     file: null
+  },
+  stickerpipe: {
+    elId: 'stickers_btn',
+
+    apiKey: '847b82c49db21ecec88c510e377b452c',
+
+    enableEmojiTab: false,
+    enableHistoryTab: true,
+    enableStoreTab: true,
+
+    userId: null,
+
+    priceB: '0.99 $',
+    priceC: '1.99 $'
   }
 };
 
