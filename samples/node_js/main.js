1156676934398766be426269b09ebd69494052ed
dimaspirit <dimaspirit@inbox.ru>
2016-04-18 17:20:14 +0300

    rewrite node_js sample  - upd gitignore  - jshintrc  - delete a log file

--- samples/node_js/main.js
+++ samples/node_js/main.js
@@ -1,48 +1,72 @@
-// var QB = require(__dirname+'/quickblox/quickblox');
-// var QB = require("../../quickblox");
-var http    = require("http");
-var url     = require("url");
-var $       = require('../../node_modules/jquery/dist/jquery');
-var QB      = require('../../js/qbMain');
-var fs      = require('fs');
-var request = require('request');
+/**
+ * Sample for Node env.
+ * Start example from root folder by command `node samples/node_js/main.js`
+ *
+ */
 
-var config = {
-  // debug: true // logs to console ON (backward compatibility)
-  // debug: {mode: 0}  // logs OFF
-  // debug: {mode: 1}, // logs to console ON
-  debug: {mode: 2, file: "app.log"}  // logs to file ON
-  // debug: {mode: [1, 2], file: "myfile.log"} // // logs to console and file ON
+/**
+ * Declaring variables
+ */
+const fs = require('fs');
+const url = require('url');
+const util = require('util');
+const http = require('http');
+const request = require('request');
+
+const QB = require('../../src/qbMain.js');
+
+/** See http://quickblox.com/developers/Javascript#Configuration */
+const CONFIG = {
+    'debug': {
+        'mode': 2, //  [1, 2] logs to console and file ON
+        'file': 'sample_debug.log'
+    }
 };
 
-var QBUser = {
- login: "nodeuser",
- password: "nodeuser"
+const CREDS = {
+    'appId': 92,
+    'authKey': 'wJHdOcQSxXQGWx5',
+    'authSecret': 'BTFsj7Rtt27DAmT'
 };
 
-QB.init(28287, 'XydaWcf8OO9xhGT', 'JZfqTspCvELAmnW', config);
+const QBUser = {
+    'login': 'nodeuser',
+    'password': 'nodeuser'
+};
+
+/**
+ * Start FUN
+ */
+QB.init(CREDS.appId, CREDS.authKey, CREDS.authSecret, CONFIG);
 
 QB.createSession(QBUser, function(err, result) {
-	if (err) {
-		console.log('QB >>> Something went wrong: ' + JSON.stringify(err));
-	} else {
-		var token   = result.token;
-			  user_id = result.id;
+    if (err) {
+        console.log('[QB - createSession]Error:', err);
+    } else {
+        /** An image for demonstrate a method 'QB.content.createAndUpload' */
+        var srcIMG = __dirname + '/wolf.jpg';
 
-    var srcIMG = 'wolf.jpg';
+        fs.stat(srcIMG, function (err, stats) {
+            fs.readFile(srcIMG, function (err, data) {
+                if (err) {
+                    throw err;
+                } else {
+                    var imgInfo = {
+                        'file': data,
+                        'name': 'image.jpg',
+                        'type': 'image/jpeg',
+                        'size': stats.size
+                    };
 
-    fs.stat(srcIMG, function (err, stats) {
-      fs.readFile(srcIMG, function (err, data) {
-        if (err) throw err;
-
-        QB.content.createAndUpload({file: data, name: 'image.jpg', type: 'image/jpeg', size: stats.size}, function(err, response){
-          if (err) {
-            console.log(err);
-          } else {
-            console.log(response);
-          }
+                    QB.content.createAndUpload(imgInfo, function(err, response){
+                        if (err) {
+                            console.log('[QB - createAndUpload]Error:', err);
+                        } else {
+                            console.log('[QB - createAndUpload]Success:', response);
+                        }
+                    });
+                }
+            });
         });
-      });
-    });
-  }
+    }
 });
