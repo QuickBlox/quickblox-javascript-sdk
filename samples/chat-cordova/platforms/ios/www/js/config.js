e99e22b3f13e7ebecfe4ad3304c52b9892efc177
Igor Khomenko <igor@quickblox.com>
2016-05-25 13:45:37 +0300

    added sample chat-cordova

---
+++ samples/chat-cordova/platforms/ios/www/js/config.js
@@ -0,0 +1,44 @@
+var QBApp = {
+  appId: 28287,
+  authKey: 'XydaWcf8OO9xhGT',
+  authSecret: 'JZfqTspCvELAmnW'
+};
+
+var config = {
+  chatProtocol: {
+    active: 2
+  },
+  debug: {
+    mode: 1,
+    file: null
+  },
+  stickerpipe: {
+    elId: 'stickers_btn',
+
+    apiKey: '847b82c49db21ecec88c510e377b452c',
+
+    enableEmojiTab: false,
+    enableHistoryTab: true,
+    enableStoreTab: true,
+
+    userId: null,
+
+    priceB: '0.99 $',
+    priceC: '1.99 $'
+  }
+};
+
+var QBUser1 = {
+        id: 6729114,
+        name: 'quickuser',
+        login: 'chatusr11',
+        pass: 'chatusr11'
+    },
+    QBUser2 = {
+        id: 6729119,
+        name: 'bloxuser',
+        login: 'chatusr22',
+        pass: 'chatusr22'
+    };
+
+QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, config);
