c3ec1d10f2a81e762b01c830a2972c0dc4ffd83b
Igor Khomenko <igor@quickblox.com>
2016-07-08 23:13:10 +0300

    fixed issuw with CO file upload under node js

--- js/modules/qbData.js
+++ js/modules/qbData.js
@@ -69,13 +69,21 @@ DataProxy.prototype = {
       formData.append('field_name', params.field_name);
       formData.append('file', params.file);
     }else{
-      formData = {};
-      formData['field_name'] = params.field_name;
-      formData['file'] = params.file;
+      formData = {
+        field_name: params.field_name,
+        file: {
+          data: params.file,
+          name: params.name
+        }
+      }
     }
 
-    this.service.ajax({url: Utils.getUrl(config.urls.data, className + '/' + params.id + '/file'), data: formData,
-                      contentType: false, processData: false, type:'POST'}, function(err, result){
+    this.service.ajax({url: Utils.getUrl(config.urls.data, className + '/' + params.id + '/file'),
+                      data: formData,
+                      contentType: false,
+                      processData: false,
+                      type:'POST',
+                      isFileUpload: true}, function(err, result){
                         if (err) { callback(err, null);}
                         else { callback (err, result); }
                       });
