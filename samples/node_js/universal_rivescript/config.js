37114d1954df70d52e2f6fc3e5cf35d7477abe8a
dimaspirit <dimaspirit@inbox.ru>
2016-09-05 17:57:47 +0300

    clean up sample 'qBot'

--- samples/node_js/universal_rivescript/config.js
+++ samples/node_js/universal_rivescript/config.js
@@ -1,7 +1,5 @@
-'use strict'
+'use strict';
 
-// Get application credentioaln on https://admin.quickblox.com
-//
 exports.CONFIG = {
     "appId": "13318",
     "authKey": "WzrAY7vrGmbgFfP",
@@ -11,6 +9,5 @@ exports.CONFIG = {
         "name": "qbot",
         "password": "mehdoh00",
         "fullname": "qbot"
-    },
-    "dialogIds": ["56cd90f8a28f9a7e7d000113", /*"573734d5a28f9a06a700008d"*/]
+    }
 };
--- samples/node_js/universal_rivescript/universal_rivescript.js
+++ samples/node_js/universal_rivescript/universal_rivescript.js
@@ -1,136 +1,138 @@
-'use strict'
+'use strict';
+
+/**
+ * Before using you need to install universal_rivescript
+ * by the command `npm i` 
+ */
 
 const CONFIG = require('./config').CONFIG;
 const QB = require('../../../src/qbMain.js');
 const RiveScript = require('rivescript');
 
-// Init QuickBlox
-//
-QB.init(CONFIG.appId, CONFIG.authKey, CONFIG.authSecret);
-
 // Init RiveScript
-//
 var riveScriptGenerator = new RiveScript();
-riveScriptGenerator.loadFile("replies.rive", loadingDone, loadingError);
 
-function loadingDone (batch_num) {
-	console.log("(RiveScript) Batch #" + batch_num + " has finished loading!");
+function loadingDone(batch_num) {
+    console.log(`[RiveScript] Batch #${batch_num} has finished loading!`);
 
-	// Now the replies must be sorted!
-	riveScriptGenerator.sortReplies();
+    riveScriptGenerator.sortReplies();
 }
 
-function loadingError (batch_num, error) {
-	console.log("(RiveScript) Error when loading files: " + error);
+function loadingError(batch_num, error) {
+    console.log(`[RiveScript] Load the batch #${batch_num} is failed`, JSON.stringify(error));
 }
 
-QB.init(CONFIG.appId, CONFIG.authKey, CONFIG.authSecret);
-
-
-QB.createSession({
-		login: CONFIG.user.name,
-		password: CONFIG.user.password
-	}, (err, res) => {
-		if (res) {
-			QB.chat.connect({userId: CONFIG.user.id, password: CONFIG.user.password}, (err, roster) => {
-				if (err) {
-					console.log('error',err);
-					process.exit(1);
-				}
-
-				QB.chat.dialog.list({type: 2}, (err, result) => {
-					if(err){
-						console.log(`can\'t get to groupcahts`);
-					}
-
-					result.items.forEach((dialog)=>{
-						QB.chat.muc.join(dialog.xmpp_room_jid);
-					});
-
-				});
-
-				QB.chat.onMessageListener = processMessage;
-
-				QB.chat.onSubscribeListener = function(userId){
-					console.log('onSubscribeListener userId', userId);
-					QB.chat.roster.confirm(userId, function(){
-						console.log('confirm subscription from user ',userId);
-					});
-				};
-
-				QB.chat.onSystemMessageListener = function(receivedMessage){
-					if(receivedMessage.extension.notification_type === '1'){
-						console.log(`user ${receivedMessage.userId} adds you to dialog`);
-						var roomJid = QB.chat.helpers.getRoomJidFromDialogId(receivedMessage.extension.dialog_id);
-						QB.chat.muc.join(roomJid);
-					}
-				};
-			});
-		}else{
-			console.log('can\'t  create session', err);
-		}
-	}
-);
-
+riveScriptGenerator.loadFile('replies.rive', loadingDone, loadingError);
 
 process.on('exit', function () {
-	QB.chat.disconnect();
+    console.log('The qbot is gone.');
+    QB.chat.disconnect();
 });
 
-function processMessage(userId, message){
 
-    if (message.type == 'groupchat') {
+/** Start fun */
+QB.init(CONFIG.appId, CONFIG.authKey, CONFIG.authSecret);
 
-		// - skip own messages in the group chat, don't replay to them
-		// - reply only when someone mentions you. For example: "@YourBotBestFriend how are you?"
-		var mentionStartIndex = -1;
-		var mentionPattern = "@" + CONFIG.user.fullname;
-		var mentionLength = mentionPattern.length;
+var qbListeners = {
+    onSubscribeListener: function onSubscribeListener(userId) {
+        console.log('[QB] onSubscribeListener. Subscribe from ${userId}');
+        
+        QB.chat.roster.confirm(userId, function() {
+            console.log(`[QB] Confirm subscription from user ${userId}`);
+        });
+    },
+    onSystemMessageListener: function onSystemMessageListener(msg) {
+        if(msg.extension.notification_type === '1'){
+            console.log(`[QB] The user ${msg.userId} adds you to dialog`);
 
-		if(message.body){
-			mentionStartIndex = message.body.indexOf(mentionPattern)
-		}
+            var roomJid = QB.chat.helpers.getRoomJidFromDialogId(msg.extension.dialog_id);
 
-		if(userId != CONFIG.user.id && mentionStartIndex >= 0){
-			// build a reply
-			var realBody;
+            QB.chat.muc.join(roomJid);
+        }
+    },
+    onMessageListener: function onMessageListener(userId, msg) {
+        var answer;
 
-			if(mentionStartIndex == 0 && message.body.substring(mentionLength, mentionLength+1) == " "){
-				realBody = message.body.substring(mentionLength+1);
-			}else{
-				realBody = "What's up? I react only for commands like this: '@YourBotBestFriend <text>'"
-			}
+        if (msg.type == 'groupchat') {
 
-			var answer = {
-				type: 'groupchat',
-				body: riveScriptGenerator.reply(userId, realBody),
-				extension: {
-					save_to_history: 1
-				}
-			};
+            // - skip own messages in the group chat, don't replay to them
+            // - reply only when someone mentions you. For example: "@YourBotBestFriend how are you?"
+            var mentionStartIndex = -1;
+            var mentionPattern = '@' + CONFIG.user.fullname;
+            var mentionLength = mentionPattern.length;
 
-			QB.chat.send(QB.chat.helpers.getRoomJidFromDialogId(message.dialog_id), answer);
-		}
-	} else if (message.type == 'chat') {
-		if(message.body){
-			var answer = {
-				type: 'chat',
-				body: riveScriptGenerator.reply(userId, message.body),
-				extension: {
-					save_to_history: 1
-				}
-			};
-			QB.chat.send(userId, answer);
-		}
-	}
-}
+            if(msg.body){
+                mentionStartIndex = msg.body.indexOf(mentionPattern);
+            }
 
-function generatePresenceSubscribed(toJid){
-	var stanza = new Client.Stanza('presence', {to: toJid, type: "subscribed"});
-	return stanza
-}
+            if(userId != CONFIG.user.id && mentionStartIndex >= 0){
+                // build a reply
+                var realBody;
 
-function generatePresenceSubscribe(toJid){
-	var stanza = new Client.Stanza('presence', {to: toJid, type: "subscribe"});
-	return stanza
-}
+                if(mentionStartIndex === 0 && msg.body.substring(mentionLength, mentionLength+1) == ' '){
+                    realBody = msg.body.substring(mentionLength + 1);
+                }else{
+                    realBody = "What's up? I react only for commands like this: '@YourBotBestFriend <text>'";
+                }
+
+                answer = {
+                    type: 'groupchat',
+                    body: riveScriptGenerator.reply(userId, realBody),
+                    extension: {
+                        save_to_history: 1
+                    }
+                };
+
+                QB.chat.send(QB.chat.helpers.getRoomJidFromDialogId(msg.dialog_id), answer);
+            }
+        } else if (msg.type == 'chat') {
+            if(msg.body){
+                answer = {
+                    type: 'chat',
+                    body: riveScriptGenerator.reply(userId, msg.body),
+                    extension: {
+                        save_to_history: 1
+                    }
+                };
+
+                QB.chat.send(userId, answer);
+            }
+        }
+    }
+ };
+
+QB.createSession({
+    login: CONFIG.user.name,
+    password: CONFIG.user.password
+}, (createSessionError, res) => {
+        if(createSessionError) {
+            console.error('[QB] createSession is failed', JSON.stringify(createSessionError));
+            process.exit(1);
+        }
+
+        QB.chat.connect({
+            userId: CONFIG.user.id,
+            password: CONFIG.user.password
+        }, (chatConnectError) => {
+            if (chatConnectError) {
+                console.log('[QB] chat.connect is failed', JSON.stringify(chatConnectError));
+                process.exit(1);
+            }
+
+            QB.chat.dialog.list({type: 2}, (dialogListError, dialogList) => {
+                if(dialogListError){
+                    console.log('[QB] dialog.list is failed', JSON.stringify(dialogListError));
+                    process.exit(1);
+                }
+
+                dialogList.items.forEach((dialog) => {
+                    QB.chat.muc.join(dialog.xmpp_room_jid);
+                });
+            });
+
+            QB.chat.onMessageListener = qbListeners.onMessageListener;
+            QB.chat.onSubscribeListener = qbListeners.onSubscribeListener;
+            QB.chat.onSystemMessageListener = qbListeners.onSystemMessageListener;
+        });
+    }
+);
