c56fe88f952bb4fe80e0f076ecab9517e0cc74af
Iegor Kozakov <kozakov.e@gmail.com>
2017-03-21 12:35:50 +0200

    fix last message preview

--- samples/newChat/js/listeners.js
+++ samples/newChat/js/listeners.js
@@ -28,13 +28,14 @@ Listeners.prototype.onMessageListener = function (userId, message) {
     if (dialog) {
         dialog.messages.unshift(msg);
 
+        dialogModule.changeLastMessagePreview(msg.chat_dialog_id, msg);
+
         var activeTab = document.querySelector('.j-sidebar__tab_link.active'),
             tabType = activeTab.dataset.type,
             dialogType = dialog.type === 1 ? "public" : "chat";
 
         if(tabType === dialogType){
             dialogModule.renderDialog(dialog, true);
-            dialogModule.changeLastMessagePreview(msg.chat_dialog_id, msg);
 
             if (dialogModule.dialogId === msg.chat_dialog_id) {
                 messageModule.renderMessage(msg, true);
