09ba4c84bc6417ce124a43a5839eba57ef94e1b9
dimaspirit <dspwww@gmail.com>
2017-05-03 19:29:09 +0300

    remove

--- samples/data/js/checkin.js
+++ samples/data/js/checkin.js
@@ -33,31 +33,3 @@ Checkin.prototype.get = function(params) {
     });
   });
 }
-
-// Places.prototype.getPlace = function(id) {
-//   return this.items.find(function(place) {
-//     return place._id === id;
-//   })
-// }
-
-// Places.prototype.sync = function() {
-//   var self = this;
-
-//   var filter = { 
-//     'sort_asc': 'created_at'
-//   };
-
-//   return new Promise(function(resolve, reject) {
-//     QB.data.list(self.className, filter, function(err, res){
-//       if (err) { 
-//         reject(err);
-//       } else {
-//         res.items.forEach(function(el) {
-//           self.items.push(el);
-//         });
-
-//         resolve();
-//       }
-//     });
-//   })
-// }
