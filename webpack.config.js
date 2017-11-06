6a306529565e2204d3b28c545c4dac706c6b9d28
vladlukhanin <vladlukhanin@gmail.com>
2017-11-06 10:31:00 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
-module.exports = {
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const webpackConfig = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   output: {
     library: 'Quickblox',
     libraryTarget: 'umd',
@@ -19,8 +22,28 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader'
       }
     ]
-  }
+  },
+  watch: NODE_ENV === 'development',
+  devtool: NODE_ENV === 'development' ? 'inline-source-map' : false,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    })
+  ]
 };
+
+if (NODE_ENV === 'production') {
+  webpackConfig.plugins.push(
+    new MinifyPlugin(true, {
+      comments: false,
+      sourceMap: module.exports.devtool
+    })
+  );
+}
+
+console.log('The webpack is starting, environment: ' + NODE_ENV);
+
+module.exports = webpackConfig;
