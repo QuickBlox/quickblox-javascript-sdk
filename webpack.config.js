e492dc41ed573c7af444d7c4dba0d482aa89f8cc
vladlukhanin <valdlukhanin@gmail.com>
2017-11-15 13:33:38 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
