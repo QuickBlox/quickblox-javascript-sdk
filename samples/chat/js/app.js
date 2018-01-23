fc5cf4d77238450071d8cad8d52875d26c2ea0b1
vladlukhanin <vladlukhanin@gmail.com>
2018-01-23 11:56:32 +0200

    The previous version of logout is returned

--- samples/chat/js/app.js
+++ samples/chat/js/app.js
@@ -57,11 +57,11 @@ App.prototype.renderDashboard = function (activeTabName) {
     listeners.setListeners();
 
     logoutBtn.addEventListener('click', function () {
-        QB.logout(function(error, result){
-            if (error) {
-                console.error('Logout was unsuccessful', error);
+        QB.users.delete(app.user.id, function(err, user){
+            if (!user) {
+                console.error('Can\'t delete user by id: '+app.user.id+' ', err);
             }
-
+            
             loginModule.isLogin = false;
             app.isDashboardLoaded = false;
 
