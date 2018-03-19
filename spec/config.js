6fd0a5e0bcc22588d83912b78c090adae6d74f80
Dima Lobanov <dspwww@gmail.com>
2018-03-19 17:44:16 +0200

    Fix EncodeURI (#319)
    Made/Proposed changes:
    
    Encode body of a request.
    Encode based on this https://en.wikipedia.org/wiki/Percent-encoding#Percent-encoding_reserved_characters
    How should this be manually tested?
    Run spec by jasmine;
    
    Does the documentation need an update?
    NO

--- spec/config.js
+++ spec/config.js
@@ -3,75 +3,38 @@
 (function() {
     'use strict';
 
-    var NEW_SERVER = false;
+    var CREDS =  {
+        'appId': 29650,
+        'authKey': 'WULOyezrmxpOgQ-',
+        'authSecret': 'TqQmBFbANJ6cfu4'
+    };
 
-    if(NEW_SERVER){
+    var QBUser1 = {
+        'id': 45267491,
+        'login': "js_specsymbols",
+        'password': "[yo^kj?]i246#(!*&d7sm/zb@xwagrv5",
+        'email': "js_specsymbols@quickblox.com"
+    };
+    var QBUser2 = {
+        'id': 26904575,
+        'login': "js_jasmine22",
+        'password': "js_jasmine22",
+        'email': "js_jasmine22@quickblox.com"
+    };
 
-      var CREDS =  {
-          'appId': 10,
-          'authKey': 'XR-B4J64ad6SGvL',
-          'authSecret': 'Nw27vzzEVfNXw47'
-      };
-
-      var QBUser1 = {
-              'id': 4275,
-              'login': "js_jasmine22",
-              'password': "js_jasmine22",
-              'email': "js_jasmine22@quickblox.com"
-          },
-          QBUser2 = {
-              'id': 4276,
-              'login': "js_jasmine222",
-              'password': "js_jasmine222"
-          };
-
-      var CONFIG = {
+    var CONFIG = {
         endpoints: {
-          api: "apikafkacluster.quickblox.com", // set custom API endpoint
-          chat: "chatkafkacluster.quickblox.com" // set custom Chat endpoint
+            api: "api.quickblox.com",
+            chat: "chat.quickblox.com"
         },
         chatProtocol: {
-          active: 2 // set 1 to use BOSH, set 2 to use WebSockets (default)
+            active: 2 // set 1 to use BOSH, set 2 to use WebSockets (default)
         },
-        'debug': {
+        debug: {
             'mode': 1,
             'file': null
         }
-      };
-
-    }else{
-      var CREDS =  {
-          'appId': 29650,
-          'authKey': 'WULOyezrmxpOgQ-',
-          'authSecret': 'TqQmBFbANJ6cfu4'
-      };
-
-      var QBUser1 = {
-              'id': 26904575,
-              'login': "js_jasmine22",
-              'password': "js_jasmine22",
-              'email': "js_jasmine22@quickblox.com"
-          },
-          QBUser2 = {
-              'id': 26904594,
-              'login': "js_jasmine222",
-              'password': "js_jasmine222"
-          };
-
-      var CONFIG = {
-        endpoints: {
-          api: "api.quickblox.com", // set custom API endpoint
-          chat: "chat.quickblox.com" // set custom Chat endpoint
-        },
-        chatProtocol: {
-          active: 2 // set 1 to use BOSH, set 2 to use WebSockets (default)
-        },
-        'debug': {
-            'mode': 1,
-            'file': null
-        }
-      };
-    }
+    };
 
     /**
      * Check Node env.
